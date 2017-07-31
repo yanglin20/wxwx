1af86fb6fa05fc33d6f97720830d98551e419a3c
Your Real Name <you@email.address>
2017-07-31 20:16:39 +0800

    7.31

--- js/index.js
+++ js/index.js
@@ -5,8 +5,17 @@ $(function() {
             swiperAnimateCache(swiper); //隐藏动画元素 
             swiperAnimate(swiper); //初始化完成开始动画
         },
-        onSlideChangeEnd: function(swiper) {
+        onSlideChangeEnd: function(swiper){
             swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+            switch (swiper.activeIndex) {
+                case 1:
+                    setTimeout(function(){
+                        $(".que2 img").css({
+                            animation: 'mysec 0.5s linear 2'
+                        })
+                    },600);//定时器时间是上一个动画执行时间
+                    break;
+            }
         }
     });
 
@@ -24,6 +33,7 @@ $(function() {
         } else {
             $(this).children('div').hide();
         }
+        $(this).siblings().children('div').hide();
         var index=$(this).parent().attr("id");
         answers[index]=$(this).children('div:visible').parent().children('span').text();
         console.log(answers);
@@ -41,11 +51,11 @@ $(function() {
             arr1.push($(this).children('span').text())
         }
         answers[index]=arr1;
-        if(arr1.length>1){
-            setTimeout(function() {
-                myswiper.slideNext();
-            }, 1000)
-        }
+        // if(arr1.length>1){
+        //     setTimeout(function() {
+        //         myswiper.slideNext();
+        //     }, 1000)
+        // }
     });
     $('#qudao button').click(function(){
         $(this).children('div').show();
@@ -54,16 +64,37 @@ $(function() {
             arr2.push($(this).children('span').text())
         }
         answers[index]=arr2;
-        if(arr2.length>1){
-            setTimeout(function() {
-                myswiper.slideNext();
-            }, 1000)
-        }
     });
+
+    //判断最后一页的背景图是什么
+    var src;
+    if(answers.sex =='男'){
+        if(answers.yusuan =="1万以下"){
+            
+        }else if(answers.yusuan == "1~3万"){
+
+        }else if(answers.yusuan == "3~5万"){
+            
+        }else if(answers.yusuan == "5~10万"){
+            
+        }else{
+            
+        }
+    }else{
+
+    }
     $('#save').click(function(){
         answers.name=$('.answer9 input').val();
         console.log(answers);
         $(".result").html(answers.name);
     })
+
+  
+    
+
+
+    
+
+
     
 })
